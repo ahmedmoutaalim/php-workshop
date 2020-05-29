91fb97f547bbf190d6965522a3e70b23e1968cac
Omar <Omar@omar.com>
2020-05-29 13:59:19 +0000

    enoncé1

---
+++ enoncé/movies.php
@@ -0,0 +1,18 @@
+<?php
+$movieName = $_GET["movieName"];
+$actorName = $_GET["actorName"];
+$movieDate = $_GET["movieDate"];
+?>
+
+
+<html>
+   <head>
+      <title>ironMan</title>
+   </head>
+<body>
+    
+    <h1><strong>information about <?php echo "$movieName"; ?></strong></h1>
+    <p><b><?php echo "$actorName"; ?></b>  starred in the movie <b><?php echo "$movieName" ;?></b>which was released in year <b><?php echo  "$movieDate";?></b>
+    </p>
+</body>
+</html>
